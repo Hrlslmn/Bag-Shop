c26352e34070580782f8c80b1219ff402702e200
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-06-27 14:20:38 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -4,19 +4,19 @@ fetch('./products.json')
   .then((response) => response.json())
   .then((json) => json.forEach((prod)=>{
     const image = document.createElement("img")
-    const container = document.createElement("div")
     const name = document.createElement("h2")
     const desc = document.createElement("h3")
     const price = document.createElement("p")
     const buyBtn = document.createElement("button")
-
-
+    const container = document.createElement("div")
+    
     buyBtn.className = "snipcart-add-item"
     image.className = "prod-image"
     price.className = "prod-price"
     container.className = "prod-container"
     desc.className = "prod-description"
     name.className = "prod-title"
+    
 
     buyBtn.setAttribute("data-item-name", prod.title)
     buyBtn.setAttribute("data-item-price", prod.price)
@@ -39,4 +39,5 @@ fetch('./products.json')
     mainContainer.appendChild(container)
   })
   )
+
 
